43996f12116f51ed2fa2e97667072e46c12f5e1c
Sekhrita <thezekro1k@gmail.com>
2023-06-07 16:26:55 -0400

    forms product correct

--- inventory_web/settings.py
+++ inventory_web/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'inventory_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lab_bioinfo',
+        'USER': 'root',
+        'PASSWORD': '5337489354760',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
